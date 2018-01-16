5262c0a4ce89bef0be07aecf06785ea51515a29f
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2018-01-16 14:08:27 -0800

    Transported some data from Excel for demo.
    Added autocomplete functionality for input fields.

--- index.js
+++ index.js
@@ -7,19 +7,160 @@ var SearchCtrl = (function () {
                     {
                         "name": "Ribs",
                         "Findings": [
-                            { "name": "Fracture" },
-                            { "name": "Bone Lesion" },
-                            { "name": "Metal bone implant or fixation hardware" }
+                            {
+                                "name": "Fracture",
+                                "Laterality Modifier": [
+                                    { "name": "Left" },
+                                    { "name": "Right" }
+                                ],
+                                "Character Modifiers": [
+                                    { "name": "Non-displaced" },
+                                    { "name": "Displaced" },
+                                    { "name": "Comminuted" },
+                                    { "name": "segmental" }
+                                ],
+                                "# Modifiers": [
+                                    { "name": "NA" }
+                                ],
+                                "Size Modifiers": [
+                                    { "name": "NA" }
+                                ]
+                            },
+                            {
+                                "name": "Bone Lesion",
+                                "Laterality Modifier": [
+                                    { "name": "Left" },
+                                    { "name": "Right" }
+                                ],
+                                "Character Modifiers": [
+                                    { "name": "Focal Lytic" },
+                                    { "name": "Focal Blastic" },
+                                    { "name": "Likely Pagets" }
+                                ],
+                                "# Modifiers": [
+                                    { "name": "NA" }
+                                ],
+                                "Size Modifiers": [
+                                    { "name": "Small" },
+                                    { "name": "Moderate" },
+                                    { "name": "Large" },
+                                    { "name": "Measures: " },
+                                ]
+                            },
+                            {
+                                "name": "Metal bone implant or fixation hardware",
+                                "Laterality Modifier": [
+                                    { "name": "Left" },
+                                    { "name": "Right" }
+                                ],
+                                "Character Modifiers": [
+                                    { "name": "Fixation wires" }
+                                ],
+                                "# Modifiers": [
+                                    { "name": "NA" }
+                                ],
+                                "Size Modifiers": [
+                                    { "name": "NA" }
+                                ]
+                            }
                         ]
                     },
                     {
                         "name": "Cervical Spine",
                         "Findings": [
-                            { "name": "Fracture" },
-                            { "name": "Bone Lesion" },
-                            { "name": "Metal bone implant or fixation hardware" },
-                            { "name": "Scoliosis" },
-                            { "name": "Spinal degenerative changes" }
+                            { 
+                                "name": "Fracture",
+                                "Laterality Modifier": [
+                                    { "name": "NA" }
+                                ],
+                                "Character Modifiers": [
+                                    { "name": "Non-displaced" },
+                                    { "name": "Moderately displaced" },
+                                    { "name": "Markedly displaced" },
+                                    { "name": "Mild compression" },
+                                    { "name": "Moderate compression" },
+                                    { "name": "Severe compression" }
+                                ],
+                                "# Modifiers": [
+                                    { "name": "NA" }
+                                ],
+                                "Size Modifiers": [
+                                    { "name": "NA" }
+                                ]
+                            },
+                            { 
+                                "name": "Bone Lesion",
+                                "Laterality Modifier": [
+                                    { "name": "NA" }
+                                ],
+                                "Character Modifiers": [
+                                    { "name": "Focal Lytic" },
+                                    { "name": "Focal Blastic" },
+                                    { "name": "Likely Pagets" }
+                                ],
+                                "# Modifiers": [
+                                    { "name": "NA" }
+                                ],
+                                "Size Modifiers": [
+                                    { "name": "Small" }, 
+                                    { "name": "Moderate" },
+                                    { "name": "Large" },
+                                    { "name": "Measures:" }
+                                ]
+                            },
+                            { 
+                                "name": "Metal bone implant or fixation hardware",
+                                "Laterality Modifier": [
+                                    { "name": "NA" }
+                                ],
+                                "Character Modifiers": [
+                                    { "name": "Vertebroplasty" },
+                                    { "name": "Fixation plate and screws" },
+                                    { "name": "Disc replacement" }
+                                ],
+                                "# Modifiers": [
+                                    { "name": "NA" }
+                                ],
+                                "Size Modifiers": [
+                                    { "name": "NA" }
+                                ]
+                            },
+                            { 
+                                "name": "Scoliosis",
+                                "Laterality Modifier": [
+                                    { "name": "NA" }
+                                ],
+                                "Character Modifiers": [
+                                    { "name": "Right convex" },
+                                    { "name": "Left convex" },
+                                    { "name": "Mild" },
+                                    { "name": "Moderate" },
+                                    { "name": "Severe" }
+                                ],
+                                "# Modifiers": [
+                                    { "name": "NA" }
+                                ],
+                                "Size Modifiers": [
+                                    { "name": "Cobb angle measures:" }
+                                ]
+                            },
+                            { 
+                                "name": "Spinal degenerative changes",
+                                "Laterality Modifier": [
+                                    { "name": "NA" }
+                                ],
+                                "Character Modifiers": [
+                                    { "name": "Mild" },
+                                    { "name": "Moderate" },
+                                    { "name": "Severe" }
+                                ],
+                                "# Modifiers": [
+                                    { "name": "NA" }
+                                ],
+                                "Size Modifiers": [
+                                    { "name": "NA" }
+                                ]
+                            }
                         ]
                     }
                 ]
@@ -47,7 +188,7 @@ var SearchCtrl = (function () {
                             { "name": "Increased reticular markings" },
                             { "name": "Increased reticulonodular markings" },
                             { "name": "findings consistent with granulomatous disease" },
-                            { "name": "findings consistent with granulomatous" },
+                            { "name": "findings consistent with granulomatous" }
                         ]
                     },
                     {
@@ -71,8 +212,17 @@ var SearchCtrl = (function () {
 
 var UICtrl = (function () {
 
+    var DOMStrings = {
+        numberModifierInput: '#number-modifiers',
+        characterInput: '#character',
+        lateralityInput: '#laterality',
+        findingsInput: '#findings',
+        subanatomyInput: '#subanatomy',
+        majorAnatomyInput: '#major-anatomry'
+    }
+
     return {
-        setupFindings: function (searchData) {
+        setupNumberModifiers: function (searchData) {
             var options = {
                 data: searchData,
                 getValue: "name",
@@ -80,14 +230,65 @@ var UICtrl = (function () {
                     match: {
                         enabled: true
                     }
+                }
+            };
+
+            $(DOMStrings.numberModifierInput).easyAutocomplete(options);
+        },
+        setupCharacter: function (searchData) {
+            var options = {
+                data: searchData,
+                getValue: "name",
+                list: {
+                    match: {
+                        enabled: true
+                    }
+                }
+            };
+
+            $(DOMStrings.characterInput).easyAutocomplete(options);
+        },
+        setupLaterality: function (searchData) {
+            var options = {
+                data: searchData,
+                getValue: "name",
+                list: {
+                    match: {
+                        enabled: true
+                    }
+                }
+            };
+
+            $(DOMStrings.lateralityInput).easyAutocomplete(options);
+        },
+        setupFindings: function (searchData) {
+            var options = {
+                data: searchData,
+                getValue: "name",
+                list: {
+                    match: {
+                        enabled: true
+                    }, 
+                    onChooseEvent: function () {
+                        var finding = $(DOMStrings.findingsInput).val();
+                        searchData.find(function (element) {
+                            if (element.name === finding) {
+                                var laterality = element["Laterality Modifier"];
+                                var character = element["Character Modifiers"];
+                                var numberModifiers = element["# Modifiers"];
+                                UICtrl.setupLaterality(laterality);
+                                UICtrl.setupCharacter(character);
+                                UICtrl.setupNumberModifiers(numberModifiers);
+                            }
+                        });
+                    }
                 },
                 theme: "plate-dark"
             };
 
-            $('#example-findings').easyAutocomplete(options);
+            $(DOMStrings.findingsInput).easyAutocomplete(options);
         },
         setupSubanatomy: function (searchData) {
-            console.log('Subanatomy values ', JSON.stringify(searchData, undefined, 2));
             var options = {
                 data: searchData,
                 getValue: "name",
@@ -96,22 +297,22 @@ var UICtrl = (function () {
                         enabled: true
                     },
                     onChooseEvent: function () {
-                        var subanatomy = $("#example-subanatomy").val();
-                        searchData.find(function(element){
-                           if (element.name === subanatomy ) {
-                               var findings = element["Findings"];
-                               UICtrl.setupFindings(findings);
-                           } 
+                        var subanatomy = $(DOMStrings.subanatomyInput).val();
+                        searchData.find(function (element) {
+                            if (element.name === subanatomy) {
+                                var findings = element["Findings"];
+                                UICtrl.setupFindings(findings);
+                            }
                         });
                     }
                 },
                 theme: "plate-dark"
             };
 
-            $("#example-subanatomy").easyAutocomplete(options);
+            $(DOMStrings.subanatomyInput).easyAutocomplete(options);
         },
         setupMajorAnatomy: function (searchData) {
-            
+
             var options = {
                 data: searchData,
                 listLocation: function (element) {
@@ -123,7 +324,7 @@ var UICtrl = (function () {
                         enabled: true
                     },
                     onChooseEvent: function () {
-                        var majorAnatomy = $("#example-major-anatomy").val();
+                        var majorAnatomy = $(DOMStrings.majorAnatomyInput).val();
 
                         searchData["Major Anatomic Region"].find(function (element) {
                             if (element.name === majorAnatomy) {
@@ -135,7 +336,7 @@ var UICtrl = (function () {
                 }
             };
 
-            $("#example-major-anatomy").easyAutocomplete(options);
+            $(DOMStrings.majorAnatomyInput).easyAutocomplete(options);
         }
     }
 })();

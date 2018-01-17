ab239e1f4a7885b84e2d51b35a56966755c5ada7
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2018-01-17 12:19:43 -0800

    Bring Annotation DataTable - DataTable generation code now included.

--- index.js
+++ index.js
@@ -480,13 +480,100 @@ var SearchCtrl = (function () {
 
 var UICtrl = (function () {
 
+    var body, table, thead, tbody, tr, td;
+
     var DOMStrings = {
+        body: 'body',
+        table: 'table',
+        thead: 'thead',
+        tbody: 'tbody',
+        th: 'th',
+        tr: 'tr',
+        td: 'td',
+        tableDiv: '.annotation-table',
         numberModifierInput: '#number-modifiers',
         characterInput: '#character',
         lateralityInput: '#laterality',
         findingsInput: '#findings',
         subanatomyInput: '#subanatomy',
         majorAnatomyInput: '#major-anatomry'
+    };
+
+    var createTblHeading = function (colNames) {
+        body = document.getElementsByTagName(DOMStrings.body)[0];
+        table = document.createElement(DOMStrings.table);
+        thead = document.createElement(DOMStrings.thead);
+        tr = document.createElement(DOMStrings.tr);
+
+        for (var i = 0; i < colNames.length; i++) {
+            var th = document.createElement(DOMStrings.th);
+            var cellText = document.createTextNode(colNames[i]);
+            th.appendChild(cellText);
+            tr.appendChild(th);
+        }
+
+        // Delete Row Th housing delete button for each row
+        var deleteRowTh = document.createElement(DOMStrings.th);
+        deleteRowTh.appendChild(document.createTextNode('Delete'));
+        tr.appendChild(deleteRowTh);
+
+        thead.appendChild(tr);
+        // table.appendChild(tr);
+        table.appendChild(thead);
+        table.id = 'annotation-table';
+        table.classList.add('table');
+        table.classList.add('table-bordered');
+        table.classList.add('responsive');
+    };
+
+    var createTblBody = function (annotationRows) {
+        tbody = document.createElement(DOMStrings.tbody);
+        // tbody.id = 'myTable';
+
+        var singleRowElements;
+        for (var i = 0; i < annotationRows.length; i++) {
+
+            singleRowElements = annotationRows[i]['row'];
+            tr = document.createElement(DOMStrings.tr);
+
+            for (var j = 0; j < singleRowElements.length; j++) {
+                td = document.createElement(DOMStrings.td);
+                var cellText = document.createTextNode(singleRowElements[j]['value']);
+
+                td.appendChild(cellText);
+                tr.appendChild(td);
+            }
+
+            var deleteBtnTd = document.createElement('td');
+            var rowDeleteBtn = document.createElement('button');
+            rowDeleteBtn.appendChild(document.createTextNode('Delete'));
+
+            deleteBtnTd.appendChild(rowDeleteBtn);
+            tr.appendChild(deleteBtnTd);
+            // Adding row one by one to tbody
+            tbody.appendChild(tr);
+        }
+        // Adding tbody to table
+        table.appendChild(tbody);
+        body.appendChild(table);
+    };
+
+    var createDT = function () {
+        table = $(DOMStrings.table).DataTable({
+            "paging": false,
+            "scrollY": "400px"
+        });
+
+        // Fix for enabling vertical scroll
+        $('.dataTables_scrollHeadInner').css("width", "100%");
+        $('div.dataTables_scrollHeadInner > table').css("width", "100%");
+
+        $('#annotation-table tbody').on('click', 'button', function () {
+            table
+                .row($(this).parents('tr'))
+                .remove()
+                .draw();
+        });
     };
 
     // Trigger focus event on input field to show list of available options
@@ -626,6 +713,32 @@ var UICtrl = (function () {
             $(DOMStrings.majorAnatomyInput).easyAutocomplete(options).focus(function () {
                 triggerFocus(DOMStrings.majorAnatomyInput);
             });
+        },
+        createTbl: function (colNames, annotationRows) {
+            createTblHeading(colNames);
+            createTblBody(annotationRows);
+            console.log(table);
+        },
+        createDataTable: function () {
+            createDT();
+        },
+        placeDT: function () {
+            // Inserting data table element inside annotation-table div
+            var dataTableDiv = document.querySelector('.dataTables_wrapper');
+            document.querySelector('.annotation-table-div').insertAdjacentElement('beforeend', dataTableDiv);
+        },
+        getRows: function () {
+            $("#getRows").on('click', function () {
+                table.rows().every(function (rowIdx, tableLoop, rowLoop) {
+                    var eachRow = this.data();
+
+                    eachRow.forEach(function (cellData) {
+                        if (!cellData.includes("Delete")) {
+                            console.log(cellData);
+                        }
+                    });
+                });
+            });
         }
     }
 })();
@@ -637,9 +750,12 @@ var controller = (function () {
             console.log('Application Started');
             var data = SearchCtrl.getSearchData();
             UICtrl.setupMajorAnatomy(data);
-
+            UICtrl.createTbl(AnnotationCtrl.getColumns(), AnnotationCtrl.getRows());
+            UICtrl.createDataTable();
+            UICtrl.placeDT();
+            UICtrl.getRows();
         }
     }
-})(SearchCtrl, UICtrl);
+})(SearchCtrl, UICtrl, AnnotationCtrl);
 
 controller.init();
